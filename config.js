81b58391ea1b6159e6e6b55c2a4e8e16d54fcc32
kepty19 <kepty.19@gmail.com>
2026-04-17 09:53:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  *
  * 例: https://script.google.com/macros/s/xxxxxxxx/exec
  */
-window.SUBMIT_URL = "";
+window.SUBMIT_URL = "https://script.google.com/macros/s/AKfycbwEfu9fPJIlK20-2DrHR9MtTcVinR_IgyIxdfB8HWgelXMbicGW1TaDXqkrryRpKi4zsg/exec";
 
 /** Apps Script 内の SECRET と一致させると送信時に検証されます（空なら無効） */
-window.SUBMIT_SECRET = "";
+window.SUBMIT_SECRET = "https://script.google.com/macros/s/AKfycbwEfu9fPJIlK20-2DrHR9MtTcVinR_IgyIxdfB8HWgelXMbicGW1TaDXqkrryRpKi4zsg/exec";
